815dbc656b92c19f131cfcca922c4d365234ccde
Michael B. McDevitt <youngchristianartist@gmail.com>
2019-04-10 20:17:45 -0500

    "Completed Step 1 of Studio"

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,41 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    //  C O N S T R U C T O R S  //
+
+    public Category(){}
+    public Category(String name){
+        this.name = name;
+    }
+
+    //  G E T T E R S  &  S E T T E R S  //
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
